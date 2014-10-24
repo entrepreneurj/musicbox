bbc21315cdcf9213b644619c9c0201f8de022cff
Joe <entrepreneurj@gmail.com>
2014-10-24 23:53:42 +0000

    Tidy's PHP and adds blank library.js
     * blank library.js with full read/write permissions needed
    
    returns gitignore for library.js

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ MusicBox
 Welcome to Music Box, your one-stop-shop for playing and sharing music on the web :)
 
 Just by putting your songs into a folder, you can create a streaming web page to listen to them anywhere you please!
+
+
+Issues:
+
+* You may need to make sure library.js is writeable if problems occur
---
+++ musicbox.php
@@ -0,0 +1,59 @@
+<?php
+require_once('vendor/getid3.php.lib/getid3/getid3.php');
+error_reporting(E_ERROR | E_WARNING | E_PARSE);
+
+$getID3 = getID3_init();
+
+$song_library=(get_songs($getID3));
+$album_library = sort_songs_by_album($song_library);
+$json = json_encode($album_library);
+$myfile = fopen("library.js", "w") or die("Unable to open file!");
+fwrite($myfile, "var library=\n ".$json); // splits js line for easy parsing later
+fclose($myfile);
+// FUNCTIONS ///
+
+
+// RECURSIVE GLOB
+// http://thephpeffect.com/recursive-glob-vs-recursive-directory-iterator/
+function rglob($pattern, $flags = 0) {
+    $files = glob($pattern, $flags); 
+    foreach (glob(dirname($pattern).'/*', GLOB_ONLYDIR|GLOB_NOSORT) as $dir) {
+        $files = array_merge($files, rglob($dir.'/'.basename($pattern), $flags));
+    }
+    return $files;
+}
+
+function get_songs($getID3) {
+    $files = array();
+    foreach (rglob("music/*.mp3") as $filename) {
+        $file=$getID3->analyze($filename);
+        $files[$filename]=array("filename" => $filename, "album" => $file["tags"]["id3v2"]["album"][0], "title" => $file["tags"]["id3v2"]["title"][0], "track" => $file["tags"]["id3v2"]["track_number"][0], "artist" => ($file["tags"]["id3v2"]["artist"][0] ?: "Various Artists"));
+    }
+    return $files;
+}
+
+function getID3_init() {
+    $PageEncoding = 'UTF-8';
+    // Initialize getID3 engine
+    $getID3 = new getID3;
+    $getID3->setOption(array('encoding' => $PageEncoding));
+
+    return $getID3;
+}
+
+function sort_songs_by_album($library) {
+    $albums = array();
+    foreach ($library as $song) {
+         $album_name = $song["album"];
+         //if the album does not exist
+        if (!$albums[$album_name]) {
+            // Create new album in list and populate name
+            $albums[$album_name] = array();
+            $albums[$album_name]["name"] = $album_name;
+        }
+        $albums[$album_name][$song["track"]] = $song;
+    }
+    return $albums;
+
+}
+?>
